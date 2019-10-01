844d2c811fc0fde4cb995a59e36fc8bcf00d1ebc
-Vishnu Sharma <vbochiw1@in.ibm.com>
2019-10-01 20:18:59 +0530

    refactor boiler plate for campaign

--- campaign-service/src/main/java/in/stackroute/umove/campaignservice/CampaignServiceApplication.java
+++ campaign-service/src/main/java/in/stackroute/umove/campaignservice/CampaignServiceApplication.java
@@ -5,16 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-import java.io.IOException;
-
 @EnableEurekaClient
 @EnableHystrix
 @SpringBootApplication
 public class CampaignServiceApplication {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(CampaignServiceApplication.class, args);
-        System.in.read();
     }
 
 }
