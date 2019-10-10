bc650d72e494f2fc5cddff0798ccb14cc03c162e
-Vishnu Sharma <vbochiw1@in.ibm.com>
2019-10-11 00:06:19 +0530

    added admin ui as spring boot ui

---
+++ admin-webapp/src/test/java/in/stackroute/umove/adminwebapp/AdminWebappApplicationTests.java
@@ -0,0 +1,16 @@
+package in.stackroute.umove.adminwebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminWebappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
