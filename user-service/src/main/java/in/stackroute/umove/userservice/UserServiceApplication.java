e27b714f85fc075792b93f23574e5511396545a2
AshwinM-134 <ashwin.m134@gmail.com>
2019-10-11 18:47:02 +0530

    refactored folder structure

--- user-service/src/main/java/in/stackroute/umove/userservice/UserServiceApplication.java
+++ user-service/src/main/java/in/stackroute/umove/userservice/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package in.stackroute.umove.userservice.main;
+package in.stackroute.umove.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
