f258ba3bc4e92bd4030739bc747bf47dea68949a
punseti1 <punseti1@in.ibm.com>
2019-10-15 17:31:37 +0530

    Handling errors

--- umove-rider-ui/src/app/map-box/map-box.page.ts
+++ umove-rider-ui/src/app/map-box/map-box.page.ts
@@ -56,6 +56,28 @@ export class MapBoxPage implements OnInit {
 
   }
 
+  isItemAvailable = false; // initialize the items with false
+
+  initializeItems(){
+    this.items = ["Ram","gopi", "dravid"];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.isItemAvailable = true;
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
   // delay = ms => new Promise(res => {
   //   setTimeout(res, ms)
   // });
