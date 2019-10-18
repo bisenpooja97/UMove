c5e1b90ae0c200d45c86799a70be0be16c810316
shagra26 <shagra26@in.ibm.com>
2019-10-18 09:54:36 +0530

    Update environment.ts

--- umove-admin-ui/src/environments/environment.ts
+++ umove-admin-ui/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8091',
+  baseUrl: 'http://172.23.234.83:8091',
   userService: 'user-service',
   userBaseApi: '/api/v1/users',
   baseUrl1: 'http://172.23.234.112:8093',
