a8623ea5f7b2d047ec998f59d18611dcea6a4779
Ubuntu <bochiwal.visnu@gmail.com>
2019-10-21 10:38:31 +0000

    completed integration and messaging

--- admin-webapp/src/main/resources/static/runtime-es2015.js
+++ admin-webapp/src/main/resources/static/runtime-es2015.js
@@ -203,7 +203,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "http://localhost:8080/adminwebapp/";
+/******/ 	__webpack_require__.p = "http://13.235.35.202:8080/adminwebapp/";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
