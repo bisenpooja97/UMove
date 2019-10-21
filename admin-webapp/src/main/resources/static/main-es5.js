2c1b86b99b16a13d250f3c9d2391da04317ad211
rucgosw1 <rucgosw1@in.ibm.com>
2019-10-21 18:18:20 +0530

    pushing to campaigns-dev

--- admin-webapp/src/main/resources/static/main-es5.js
+++ admin-webapp/src/main/resources/static/main-es5.js
@@ -214,7 +214,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/ashwin/umove/umove-admin-ui/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /home/ruchi/umove/umove-admin-ui/src/main.ts */"./src/main.ts");
 
 
 /***/ })
--- admin-webapp/src/main/resources/static/styles-es2015.js
+++ admin-webapp/src/main/resources/static/styles-es2015.js
@@ -557,7 +557,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/ashwin/umove/umove-admin-ui/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /home/ruchi/umove/umove-admin-ui/src/styles.css */"./src/styles.css");
 
 
 /***/ })
