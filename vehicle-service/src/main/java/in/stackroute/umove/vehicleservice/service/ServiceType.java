44e67fcce14e360008985206c97f6bb5a2b2252d
punseti1 <punseti1@in.ibm.com>
2019-10-23 19:54:16 +0530

    refactoring master

--- vehicle-service/src/main/java/in/stackroute/umove/vehicleservice/service/ServiceType.java
+++
@@ -1,7 +0,0 @@
-package in.stackroute.umove.vehicleservice.service;
-
-import in.stackroute.umove.vehicleservice.model.Type;
-
-public interface ServiceType {
-    public void addType(Type type);
-}
--- vehicle-service/src/test/java/in/stackroute/umove/vehicleservice/VehicleServiceApplicationTests.java
+++ vehicle-service/src/test/java/in/stackroute/umove/vehicleservice/VehicleServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package in.stackroute.umove.vehicleservice;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ public class VehicleServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	    Assert.assertEquals(true, true);
 	}
 
 }
