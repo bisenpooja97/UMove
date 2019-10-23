20d1552f41dd914f847f95a5fbd8e5f2e9ebe3df
Pooja <bisenpooja97@gmail.com>
2019-10-23 19:48:33 +0530

    Refactoring scanner code

--- umove-rider-ui/src/app/qrcode-scanner/qrcode-scanner.page.ts
+++ umove-rider-ui/src/app/qrcode-scanner/qrcode-scanner.page.ts
@@ -29,46 +29,46 @@ export class QrcodeScannerPage implements OnInit {
 
   ngOnInit() {
 
-    // this.configurationService.getConfigurationDetailsByName('autocancelTime')
-    //   .then(response => {
-    //     console.log('Configuration details: ', response);
-    //     this.configuration = JSON.parse(response.data).data;
-    //     console.log(this.configuration);
-    //     this.timerTimeInMins = this.configuration.value;
-    //     console.log('Auto-cancel time is ', this.timerTimeInMins);
-    //   });
-
-    this.rideService.getRideDetailsByUserIdNStatus('786', 'Confirmed')
+    this.configurationService.getConfigurationDetailsByName('autocancelTime')
       .then(response => {
-        console.log('Booking details in scanner: ', response);
-        this.ride = JSON.parse(response.data).data;
-        console.log(this.ride);
-        // this.rideBookedAt = new Date(this.ride.bookedAt);
-        // console.log('Ride booked at ', this.rideBookedAt);
-        // console.log(typeof (this.rideBookedAt));
-        // // this.timerTime = this.ride.timer;
-        // console.log('Timer time in minutes is ', this.timerTimeInMins);
-        // this.rideBookedAt.setMinutes(this.rideBookedAt.getMinutes() + this.timerTimeInMins);
-        // console.log('Autocancel time is ', this.rideBookedAt);
-        // this.rightNow = new Date();
-        // console.log(typeof (this.rightNow));
-        // console.log('RightNow time is ', this.rightNow);
-        // this.timerSeconds = (this.rideBookedAt.getTime() - this.rightNow.getTime()) / 1000;
-        // console.log('Scanner timer', this.timerSeconds);
-        // console.log(this.timerSeconds);
-        // if (this.timerSeconds > 0) {
-        //   this.timerSettings = {
-        //     animate: 'fade',
-        //     targetTime: this.timerSeconds,
-        //     maxWheel: 'minutes',
-        //     minWidth: 100,
-        //     autostart: true,
-        //     buttons: [],
-        //     showOnFocus: false
-        //   };
-        // } else {
-        //   this.timerFinished();
-        // }
+        console.log('Configuration details: ', response);
+        this.configuration = JSON.parse(response.data).data;
+        console.log(this.configuration);
+        this.timerTimeInMins = this.configuration.value;
+        console.log('Auto-cancel time is ', this.timerTimeInMins);
+
+        this.rideService.getRideDetailsByUserIdNStatus('786', 'Confirmed')
+          .then(response1 => {
+            console.log('Booking details in scanner: ', response1);
+            this.ride = JSON.parse(response1.data).data;
+            console.log(this.ride);
+            this.rideBookedAt = new Date(this.ride.bookedAt + 'Z');
+            console.log('Ride booked at ', this.rideBookedAt);
+            console.log(typeof (this.rideBookedAt));
+            // // this.timerTime = this.ride.timer;
+            console.log('Timer time in minutes is ', this.timerTimeInMins);
+            this.rideBookedAt.setMinutes(this.rideBookedAt.getMinutes() + this.timerTimeInMins);
+            console.log('Autocancel time is ', this.rideBookedAt);
+            this.rightNow = new Date();
+            console.log(typeof (this.rightNow));
+            console.log('RightNow time is ', this.rightNow);
+            this.timerSeconds = (this.rideBookedAt.getTime() - this.rightNow.getTime()) / 1000;
+            console.log('Scanner timer', this.timerSeconds);
+            console.log(this.timerSeconds);
+            if (this.timerSeconds > 0) {
+              this.timerSettings = {
+                animate: 'fade',
+                targetTime: this.timerSeconds,
+                maxWheel: 'minutes',
+                minWidth: 100,
+                autostart: true,
+                buttons: [],
+                showOnFocus: false
+              };
+            } else {
+              this.timerFinished();
+            }
+          });
       });
 
     this.barcodeScanner.scan().then(qrCodeData => {
@@ -81,26 +81,28 @@ export class QrcodeScannerPage implements OnInit {
         this.rideService.presentToast('Wrong QR Code.', 3000);
         this.router.navigateByUrl('ride-booking-details');
       }
-      this.rideService.startRideById(this.ride._id, this.vehicleNumber).then(response => {
-        console.log('response', response);
-        this.ride = JSON.parse(response.data).data;
-        this.router.navigateByUrl('ride-details');
-      });
+      if (this.vehicleNumber != undefined) {
+        this.rideService.startRideById(this.ride._id, this.vehicleNumber).then(response => {
+          console.log('response', response);
+          this.ride = JSON.parse(response.data).data;
+          this.router.navigateByUrl('ride-details');
+        });
+      }
     });
   }
 
-  // timerFinished() {
-  //   this.rideService.cancelRideById(this.ride._id).then(data => {
-  //     console.log('response of autocancel ride: ', data);
-  //   });
-  //   mobiscroll.alert({
-  //     title: 'Your ride is autocancelled',
-  //     message: 'Please book a new ride.',
-  //     callback: () => {
-  //       // Apply the url of home page
-  //       this.router.navigateByUrl('ride-booking-details');
-  //     }
-  //   });
-  // }
+  timerFinished() {
+    this.rideService.cancelRideById(this.ride._id).then(data => {
+      console.log('response of autocancel ride: ', data);
+      mobiscroll.alert({
+        title: 'Your ride is autocancelled',
+        message: 'Please book a new ride.',
+        callback: () => {
+          // Apply the url of home page
+          this.router.navigateByUrl('ride-booking-details');
+        }
+      });
+    });
+  }
 
 }
