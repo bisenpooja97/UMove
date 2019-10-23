da8f008617944f7ef5fd49ec16895fcc6b5e0c03
punseti1 <punseti1@in.ibm.com>
2019-10-23 20:06:28 +0530

    refactoring env.ts fiel

--- umove-rider-ui/src/environments/environment.ts
+++ umove-rider-ui/src/environments/environment.ts
@@ -28,12 +28,3 @@ export const environment = {
     appId: '1:505919961682:web:a1ad48542500c354403d8b'
   },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
