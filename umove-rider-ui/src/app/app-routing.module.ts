30ebdffb8f295c7b43175a69bd1cf621fc5dfc7b
-Vishnu Sharma <vbochiw1@in.ibm.com>
2019-10-25 18:53:31 +0530

    completed integration with rider ui and supervisior ui

--- umove-rider-ui/src/app/app-routing.module.ts
+++ umove-rider-ui/src/app/app-routing.module.ts
@@ -10,7 +10,6 @@ const routes: Routes = [
    { path: 'bikelist', component: BikeListPage},
     { path: 'drop', component: DropPage},
     { path: 'confirm-ride-detail', loadChildren: './confirm-ride-detail/confirm-ride-detail.module#ConfirmRideDetailPageModule' },
-    { path: 'dummy-promocode', loadChildren: './dummy-promocode/dummy-promocode.module#DummyPromocodePageModule' },
     { path: 'ride-booking-details', loadChildren: './ride-booking-details/ride-booking-details.module#RideBookingDetailsPageModule' },
     { path: 'ride-details', loadChildren: './ride-details/ride-details.module#RideDetailsPageModule' },
     // tslint:disable-next-line:max-line-length
--- umove-rider-ui/src/app/ride-details/ride-details.page.ts
+++ umove-rider-ui/src/app/ride-details/ride-details.page.ts
@@ -3,7 +3,7 @@ import { RideService } from '../service/ride.service';
 import {ActivatedRoute, NavigationExtras, Router} from '@angular/router';
 import { Ride } from '../model/ride';
 import { Zone } from '../model/zone';
-
+import {Storage} from '@ionic/storage';
 @Component({
   selector: 'app-ride-details',
   templateUrl: './ride-details.page.html',
@@ -15,24 +15,27 @@ export class RideDetailsPage implements OnInit {
   sizeOfDestinationZones: number;
   destinationZone: Zone;
   destinationZoneChange: Zone;
+  key = 'details';
 
-  constructor(private rideService: RideService, private route: ActivatedRoute, private router: Router) {
+  constructor(private rideService: RideService, private route: ActivatedRoute, private router: Router, private storage: Storage) {
   }
 
   ngOnInit() {
 
-    this.rideService.getRideDetailsByUserIdNStatus('5d8bbc0da6e87d5404aa1921', 'Started')
-    .then(response => {
-      console.log('Ride details: ', response);
-      this.ride = JSON.parse(response.data).data;
-      this.sizeOfDestinationZones = this.ride.destinationZones.length;
-      this.destinationZone = this.ride.destinationZones[this.sizeOfDestinationZones - 1];
+    this.storage.get(this.key).then(value => {
+      this.rideService.getRideDetailsByUserIdNStatus(value.id, 'Started')
+          .then(response => {
+            console.log('Ride details: ', response);
+            this.ride = JSON.parse(response.data).data;
+            this.sizeOfDestinationZones = this.ride.destinationZones.length;
+            this.destinationZone = this.ride.destinationZones[this.sizeOfDestinationZones - 1];
+          });
     });
-
     this.route.queryParams.subscribe(params => {
-      if (this.router.getCurrentNavigation().extras.queryParams) {
-        this.destinationZoneChange = this.router.getCurrentNavigation().extras.queryParams.special;
-        console.log('destinationZoneChange: ', this.destinationZoneChange);
+      console.log('params', params);
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.destinationZoneChange = this.router.getCurrentNavigation().extras.state.dropZone;
+        console.log('Changed Destination Zone', this.destinationZoneChange);
         this.rideService.changeDestinationOfUser(this.destinationZoneChange, this.ride._id).then(responseData => {
           console.log('response of change destination: ', responseData);
           this.ride = JSON.parse(responseData.data).data;
@@ -43,13 +46,20 @@ export class RideDetailsPage implements OnInit {
         });
       }
     });
+    // this.route.queryParams.subscribe(params => {
+    //   if (this.router.getCurrentNavigation().extras.queryParams) {
+    //     this.destinationZoneChange = this.router.getCurrentNavigation().extras.queryParams.specials;
+    //     console.log('destinationZoneChange: ', this.destinationZoneChange);
+    //
+    //   }
+    // });
   }
 
   // calling service method to change destination of ride
   changeDestination() {
     const navigationExtras: NavigationExtras = {
       state: {
-          page:'change-drop'
+          page: 'change-drop'
       }
     };
     this.router.navigate(['drop'], navigationExtras);
