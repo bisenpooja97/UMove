59a62c528c89fe121c8254c43bc2c223287dce15
Pooja <bisenpooja97@gmail.com>
2019-10-29 17:30:15 +0530

    timer settings value is validated

--- umove-admin-ui/src/app/settings/settings.component.ts
+++ umove-admin-ui/src/app/settings/settings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SettingsService } from './service/settings.service';
+import { NotificationService } from '../shared/notification.service';
 
 @Component({
   selector: 'app-settings',
@@ -11,7 +12,8 @@ export class SettingsComponent implements OnInit {
   autocancelTimeValue: number;
   cancelThresholdTimeValue: number;
 
-  constructor(private settingsService: SettingsService) { }
+  constructor(private settingsService: SettingsService,
+              private notificationService: NotificationService) { }
 
   ngOnInit() {
     this.settingsService.getConfigurationByName('autocancelTime').subscribe(response => {
@@ -30,7 +32,13 @@ export class SettingsComponent implements OnInit {
 
   onTimeChange(name: string, value: number) {
     this.settingsService.updateConfigurationByName(name, value).subscribe(response => {
+      this.notificationService.success('Time Updated successfully!!');
       console.log('Response of updated time: ', response);
+    },
+    error => {
+     if ( error.status === 400) {
+      this.notificationService.warn('Time cannot be Updated!!');
+     }
     });
   }
 
