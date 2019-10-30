a218adff10d51af2cb8acc112d0aba58ff6252b2
punseti1 <punseti1@in.ibm.com>
2019-10-30 09:59:38 +0530

    Refactored url to domain

--- umove-rider-ui/src/environments/environment.prod.ts
+++ umove-rider-ui/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   map: 'pk.eyJ1IjoicHVuaXRzZXRpYSIsImEiOiJjazBjOTY0cGcwMDI0M2RtbThsbWEyaXhnIn0.DrloATvnGeowNnIPanQT4g',
-  baseUrl : 'http://172.23.234.75:8080/',
+  baseUrl : 'http://umove-dev.stackroute.io:8761/',
   zoneService : '/zoneservice/api/v1',
   vehicleService: '/vehicleservice/api/v1',
   tomTomUrl : 'https://api.tomtom.com/search/2/structuredGeocode.json?countryCode=IND&streetName=',
