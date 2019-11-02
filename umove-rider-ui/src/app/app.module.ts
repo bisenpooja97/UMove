bb5f921c01379df6c8cb910e29abe85bb4fae0b7
-Vishnu Sharma <vbochiw1@in.ibm.com>
2019-11-02 20:33:27 +0530

    added notificationservice

--- umove-rider-ui/src/app/app.module.ts
+++ umove-rider-ui/src/app/app.module.ts
@@ -31,6 +31,7 @@ import {UserPaymentMethodService} from './service/user-payment-method/user-payme
 import {IonicStorageModule} from '@ionic/storage';
 import {CustomerErrorHandler} from "./customer-error-handler";
 import {CampaignService} from './service/campaign-service/campaign.service';
+import {FCM} from "@ionic-native/fcm/ngx";
 
 @NgModule({
   declarations: [AppComponent, BikeListPage, DropPage],
@@ -63,6 +64,7 @@ import {CampaignService} from './service/campaign-service/campaign.service';
     SplashScreen,
     HttpClient,
     HTTP,
+      FCM,
     UserProfileServiceService,
     UserPaymentMethodService,
       CampaignService,
